2880c3206c8a5711f5112d7dab0af0344ecc3d43
sohamgadhave <mas227112@iitd.ac.in>
2022-10-05 23:55:46 -0700

    ADD: Assignment 4 question 7 and 8

--- precedence.c
+++ precedence.c
@@ -3,6 +3,7 @@ int a = 1, b = 2, c = 3;
 int f(void);
 
 int main(){
+    printf("a = %d\tb = %d\tc = %d\n",a, b, c);
     printf("%d\n",f());
     printf("a = %d\tb = %d\tc = %d\n",a, b, c);
     return 0;
