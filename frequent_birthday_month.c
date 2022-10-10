842cea96b687484324ed0d07a2b4e1cab4a2f93b
soham.gadhave <soham.gadhave@somaiya.edu>
2022-10-10 18:56:32 +0530

    revise birthday frequency question

--- frequent_birthday_month.c
+++ frequent_birthday_month.c
@@ -1,25 +1,60 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
 #define MONTHS 12
 
-void main() {
-  int number_of_students = 10, birthday_frequency[MONTHS], month = 0;
+int getMonth(char []);
+char months[][MONTHS] = {
+  "january",
+  "february",
+  "march",
+  "april",
+  "may",
+  "june",
+  "july",
+  "august",
+  "september",
+  "october",
+  "november",
+  "december"
+};
 
-  //Initializing the array to all 0's
-  for(int i = 0; i < MONTHS; i++)
-    birthday_frequency[i] = 0;
+void main() {
+  int number_of_students, birthday_frequency[MONTHS] = {0};
+  char month[10];
+
+  printf("Enter the number of students: \n");
+  scanf("%d", &number_of_students);
+  char flush;
+  scanf("%c", &flush);
 
   for(int i = 0; i < number_of_students; i++) {
-    printf("Enter the number of birthday month: ");
-    scanf("%d", &month);
-    birthday_frequency[month - 1]++;
+    printf("Enter the birthday month: ");
+    gets(month);
+    int month_number = getMonth(month);
+    if(month_number == -1) {
+      printf("Enter the birthday month properly with all letters as lower case.\n");
+      i--;
+      continue;
+    }
+    birthday_frequency[month_number]++;
   }
 
   //Finding Max of the array
-  int max = birthday_frequency[0], max_month = 0;
+  int max = birthday_frequency[0];
   for(int i = 1; i < MONTHS; i++)
-    if(max < birthday_frequency[i]) {
+    if(max < birthday_frequency[i])
       max = birthday_frequency[i];
-      max_month = i + 1;
-    }
-  printf("Maximum birthdays are in the month: %d\n", max_month);
+
+  printf("Maximum birthdays are in the month or month(s): \n");
+  
+  for(int i = 0; i < MONTHS; i++)
+    if(birthday_frequency[i] == max)
+      printf("%s\n", months[i]);
+}
+
+int getMonth(char month[]) {
+  for(int i = 0; i < MONTHS; i++)
+    if(strcmp(months[i], month) == 0)
+      return i;
+  return -1;
 }
